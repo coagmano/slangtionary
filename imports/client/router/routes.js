ea5c6724a4e9b3df955347d0fc8fa5460b52cb90
Fred Stark <fred@displayground.com.au>
2018-02-03 21:47:03 +1100

    Fix issues

--- imports/client/router/routes.js
+++ imports/client/router/routes.js
@@ -23,8 +23,36 @@ FlowRouter.route('/', {
   },
   whileWaiting() {
     this.render('_mainLayout', 'loading');
-  }
+  },
 });
+
+FlowRouter.route('/about', {
+  name: 'about',
+  waitOn() {
+    return import('/imports/client/about/about.js');
+  },
+  whileWaiting() {
+    this.render('_mainLayout', 'loading');
+  },
+  action() {
+    this.render('_mainLayout', 'about');
+  },
+});
+
+
+FlowRouter.route('/contact', {
+  name: 'contact',
+  waitOn() {
+    return import('/imports/client/contact/contact.js');
+  },
+  whileWaiting() {
+    this.render('_mainLayout', 'loading');
+  },
+  action() {
+    this.render('_mainLayout', 'contact_slangtionary');
+  },
+});
+
 
 // 404 route (catch all)
 FlowRouter.route('*', {
